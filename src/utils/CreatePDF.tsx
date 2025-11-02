ff2b6bbf7065d5c9c6fd8357ca533845c0bb919d
Cian Dally <cpdally3@gmail.com>
2025-11-02 13:18:01 +0000

    Added Anima logo and font styling to CreatePDF

--- src/utils/CreatePDF.tsx
+++ src/utils/CreatePDF.tsx
@@ -2,6 +2,8 @@ import jsPDF from 'jspdf';
 
 export const createPDF = async (firstName: string, lastName: string) => {
   const doc = new jsPDF();
+  const pageWidth = doc.internal.pageSize.getWidth();
+  const pageHeight = doc.internal.pageSize.getHeight();
 
   const image = new Image();
   image.src = '/ThankYouImage.png';
@@ -9,18 +11,32 @@ export const createPDF = async (firstName: string, lastName: string) => {
   await new Promise((resolve) => {
     image.onload = resolve;
   });
-  
-  const pageWidth = doc.internal.pageSize.getWidth();
-  const pageHeight = doc.internal.pageSize.getHeight();
-  
+
   const imageWidth = pageWidth;
   const imageHeight = (image.height * imageWidth) / image.width;
 
   doc.addImage(image, 'PNG', 0, 0, imageWidth, imageHeight);
 
   const message = `Thank you for supporting us, ${firstName}!`;
+  
+  doc.setFontSize(30);
+  doc.setFont('Poppins', 'bold');
 
   doc.text(message, pageWidth / 2, pageHeight / 2, { align: 'center' });
+
+  const logo = new Image();
+  logo.src = '/AnimaLogo.png';
+
+  await new Promise((resolve) => {
+    logo.onload = resolve;
+  });
+
+  const logoWidth = 60;
+  const logoHeight = (logo.height * logoWidth) / logo.width;
+  const logoX = (pageWidth - logoWidth) / 2;
+  const logoY = pageHeight - logoHeight - 10;
+
+  doc.addImage(logo, 'PNG', logoX, logoY, logoWidth, logoHeight);
 
   doc.save(`thank-you-from-anima.pdf`);
 };
